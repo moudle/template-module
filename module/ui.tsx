af57ff044f48f015e717bc736a01734e7ff1d2d8
Rio <riochr17@gmail.com>
2026-05-17 18:53:38 +0700

    update start

--- module/ui.tsx
+++ module/ui.tsx
@@ -2,6 +2,10 @@ import { LoginPage } from "./page/LoginPage";
 import { RegisterPage } from "./page/RegisterPage";
 
 export const LIST_PAGE_USER = {
-  '/login': <LoginPage />,
-  '/register': <RegisterPage />
+  '/login': {
+    Component: LoginPage
+  },
+  '/register': {
+    Component: RegisterPage
+  }
 }
