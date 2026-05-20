c7f1dcf2fed86b084aa5573131c6e370f4a1cf4a
Rio <riochr17@gmail.com>
2026-05-20 14:13:38 +0700

    update

---
+++ module/config-ui.tsx
@@ -0,0 +1,6 @@
+export let CONFIG_UI_USER = {
+  login_redirect_path: '/login-success',
+  register_redirect_path: '/register-success',
+  has_logged_in_redirect_path: '/has-logged-in',
+  logout_redirect_path: '/logout-success'
+}
